5a79626d532e6fbde39953d4c9cc653316885746
imsidthakur <106941477+imsidthakur@users.noreply.github.com>
2024-10-01 07:57:49 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -15,7 +15,7 @@ public class Main {
   @Override
   public String meth()
   {
-    return "Method is overridden in Extended class B";
+    return "Method is overridden in Extendened class B";
   }
 }
 
