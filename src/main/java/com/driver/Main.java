c0e5a4b42b7065a2a33e2702b31ab8b9ecd3a910
imsidthakur <106941477+imsidthakur@users.noreply.github.com>
2024-10-01 07:27:56 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,14 +1,14 @@
 package com.driver;
 
-
-class A{
+public class Main {
+  static class A{
   public String meth()
   {
     return "Invoking method from class A";
   }
 }
 
-class B extends A{
+ static class B extends A{
   @Override
   public String meth()
   {
@@ -16,7 +16,6 @@ class B extends A{
   }
 }
 
-public class Main {
   public static void main(String []args)
   {
     B obj = new B();
